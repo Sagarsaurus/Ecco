449a567dece5390b9eff3c72defb4255cdd094db
Sunny Deo <Deo.Abhishek4@gmail.com>
2014-11-26 20:51:47 -0500

    Added TabLayout

--- Ecco/gen/com/application/ecco/R.java
+++ Ecco/gen/com/application/ecco/R.java
@@ -1097,12 +1097,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050049;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int button1=0x7f05003d;
-        public static final int button2=0x7f050046;
+        public static final int button2=0x7f050047;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1110,7 +1110,7 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int editText1=0x7f050048;
-        public static final int editText2=0x7f050047;
+        public static final int editText2=0x7f050046;
         public static final int edit_query=0x7f050030;
         public static final int email=0x7f05003f;
         public static final int end=0x7f050013;
@@ -1127,6 +1127,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int pager=0x7f050045;
         public static final int password=0x7f05003e;
         public static final int phone=0x7f050040;
         public static final int progress_circular=0x7f050018;
@@ -1142,7 +1143,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int sendFriendRequest=0x7f050045;
+        public static final int sendFriendRequest=0x7f050049;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1211,7 +1212,9 @@ containing a value of this type.
         public static final int activity_create_user=0x7f030018;
         public static final int activity_home=0x7f030019;
         public static final int activity_main=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int request_list=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int user_profile=0x7f03001d;
     }
     public static final class menu {
         public static final int create_user=0x7f0c0000;
@@ -1221,11 +1224,11 @@ containing a value of this type.
         public static final int main=0x7f0c0004;
     }
     public static final class string {
-        public static final int Email=0x7f0a001c;
-        public static final int Login=0x7f0a0012;
-        public static final int Password=0x7f0a0011;
-        public static final int Phone=0x7f0a001d;
-        public static final int Username=0x7f0a0010;
+        public static final int Email=0x7f0a001f;
+        public static final int Login=0x7f0a0015;
+        public static final int Password=0x7f0a0014;
+        public static final int Phone=0x7f0a0020;
+        public static final int Username=0x7f0a0013;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1266,21 +1269,24 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_example=0x7f0a001a;
+        public static final int action_example=0x7f0a001d;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
-        public static final int createUser=0x7f0a0013;
-        public static final int emailhint=0x7f0a001e;
+        public static final int createUser=0x7f0a0016;
+        public static final int emailhint=0x7f0a0021;
         public static final int hello_world=0x7f0a000e;
-        public static final int navigation_drawer_close=0x7f0a0019;
-        public static final int navigation_drawer_open=0x7f0a0018;
-        public static final int phonehint=0x7f0a001f;
-        public static final int title_activity_create_user=0x7f0a001b;
-        public static final int title_activity_friends=0x7f0a0020;
-        public static final int title_activity_home=0x7f0a0014;
-        public static final int title_section1=0x7f0a0015;
-        public static final int title_section2=0x7f0a0016;
-        public static final int title_section3=0x7f0a0017;
+        public static final int navigation_drawer_close=0x7f0a001c;
+        public static final int navigation_drawer_open=0x7f0a001b;
+        public static final int pass=0x7f0a0011;
+        public static final int phonehint=0x7f0a0022;
+        public static final int send_request=0x7f0a0012;
+        public static final int title_activity_create_user=0x7f0a001e;
+        public static final int title_activity_friends=0x7f0a0023;
+        public static final int title_activity_home=0x7f0a0017;
+        public static final int title_section1=0x7f0a0018;
+        public static final int title_section2=0x7f0a0019;
+        public static final int title_section3=0x7f0a001a;
+        public static final int user=0x7f0a0010;
     }
     public static final class style {
         /** 
--- Ecco/src/com/application/ecco/HomeActivity.java
+++ Ecco/src/com/application/ecco/HomeActivity.java
@@ -1,30 +1,68 @@
 package com.application.ecco;
 
 import com.loopj.android.http.*;
-
 import org.apache.http.Header;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
-
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class HomeActivity extends ActionBarActivity {
+@SuppressLint("NewApi")
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class HomeActivity extends FragmentActivity implements ActionBar.TabListener {
 	final static String EXTRA_MESSAGE = "com.application.ecco.MESSAGE";
 	JSONObject json;
+	
+	PagerAdapter adapter;
+	ViewPager pager;
+	
+	String[] title = { "User Profile", "Request List" };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		adapter = new PagerAdapter(getSupportFragmentManager());
+		
+		final ActionBar bar = getActionBar();
+		
+		bar.setHomeButtonEnabled(false);
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		pager = (ViewPager)findViewById(R.id.pager);
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				bar.setSelectedNavigationItem(position);
+			}
+		});
+		
+		for (String name : title) {
+			bar.addTab(bar.newTab().setText(name)
+					.setTabListener((TabListener) this));
+		}
 	}
 
 	@Override
@@ -44,6 +82,60 @@ public class HomeActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public static class PagerAdapter extends FragmentPagerAdapter {
+
+		public PagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int index) {
+			switch (index) {
+			case 0:
+				return new ProfileFragment();
+			case 1:
+				return new RequestListFragment();
+			}
+			return null;
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+		
+	}
+	
+	public static class ProfileFragment extends Fragment {
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View root = inflater.inflate
+					(R.layout.user_profile, container, false);
+			root.findViewById(R.id.button1)
+				.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// methods need to be static
+						// sendRequest(v);
+					}
+				});
+			return root;
+		}
+		
+	}
+	
+	public static class RequestListFragment extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View root = inflater.inflate
+					(R.layout.request_list, container, false);
+			return root;
+		}
 	}
 	
 	public void sendRequest(View view) {
@@ -74,6 +166,20 @@ public class HomeActivity extends ActionBarActivity {
 			}
 
     	});
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// go to the view
+		pager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 	}
 
 }
